d295ed1f9bf986d29ff0b183bb6f2e04af43d6a0
alaakouki <119391046+alaakouki@users.noreply.github.com>
2023-02-02 18:25:32 -0800

    updating fetch function

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 // open weather API link :
-// var ApiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=f6b53d11e4e1d813df0a764640a0b00eg";
+var requestApiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=f6b53d11e4e1d813df0a764640a0b00eg";
+
+var responseText = document.getElementById("response-text");
 
 
-
-
-
-// var requestUrl = 'https://api.github.com/orgs/nodejs/repos?per_page=5';
-
-// var responseText = document.getElementById('response-text');
-
-// function getApi(requestUrl) {
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       console.log(response);
-//       if (response.status === 200) {
 //         responseText.textContent = response.status;
-//       }
-//       return response.json();
-//   });
-// }
-
-// getApi(requestUrl);
 
 
 
+
+function getApi(requestApiUrl) {
+    fetch(requestApiUrl)
+    .then ( function (response) {
+        console.log(response);
+        if (response.status === 200) {
+            console.log(response.status);
+        }
+        return response.json();
+    })
+}
+getApi(requestApiUrl);
 
 
 
